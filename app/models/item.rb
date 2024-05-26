f84c41b6a887bfc5dffae2833723eda31974863f
441daichi <guruguru441@gmail.com>
2024-05-26 18:50:57 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
 
   has_one_attached :image
 
