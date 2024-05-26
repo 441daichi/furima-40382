93c7c2f3760d42b6c734b21305cc48100a429a90
441daichi <guruguru441@gmail.com>
2024-05-26 18:59:42 +0900

    itemモデル内のバリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,14 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :image, prisence: true do
+  validates :name, prisence: true do
+  validates :explanation, prisence: true do
+  validates :category_id, prisence: true do
+  validates :item_status_id, prisence: true do
+  validates :shipping_cost_id, prisence: true do
+  validates :prefecture_id, prisence: true do
+  validates :shipping_date_id, prisence: true do
+  validates :price, prisence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } do
+
 end
