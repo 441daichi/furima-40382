c5e83bb4a353ee372ceb06a4c3746ccd9dc6373f
441daichi <guruguru441@gmail.com>
2024-06-01 14:42:31 +0900

    レビュー後の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
   validates :shipping_date_id, presence: true, numericality: { other_than: 0 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  def sold_out?
-    self.order.present?
-  end
+  # def sold_out?
+  #   self.order.present?
+  # end
 end
