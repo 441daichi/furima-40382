511ebe1d75c1da78357e8c82219e70abf977c342
441daichi <guruguru441@gmail.com>
2024-06-10 21:17:36 +0900

    フォーム表示エラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,18 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
   
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,4 +1,4 @@
-class OderForm
+class OrderForm
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
