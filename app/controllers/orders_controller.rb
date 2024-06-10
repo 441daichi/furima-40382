d613bf56956b4b074e9e91d637cf18d8053c7a90
441daichi <guruguru441@gmail.com>
2024-06-11 06:19:54 +0900

    エラーメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
-    @order_form.save
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
