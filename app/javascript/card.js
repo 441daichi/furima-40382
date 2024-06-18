272e5d8b672b7b6d334b6d142a08e20ae110a479
441daichi <guruguru441@gmail.com>
2024-06-18 16:37:46 +0900

    公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,10 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const payjp = Payjp('pk_test_84d496bedca377ebc2a3a6bb')
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    console.log("フォーム送信時にイベント発火")
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);
